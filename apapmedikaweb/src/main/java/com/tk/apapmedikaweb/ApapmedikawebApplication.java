d3b11edfa0a88d5e1511074fa2d57498e6abbdc9
Rizvanu Satrio Nugroho <rizvanu.satrio@ui.ac.id>
2024-11-07 16:01:52 +0700

    feat: initialize repo

---
+++ apapmedikaweb/src/main/java/com/tk/apapmedikaweb/ApapmedikawebApplication.java
@@ -0,0 +1,13 @@
+package com.tk.apapmedikaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApapmedikawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApapmedikawebApplication.class, args);
+	}
+
+}
---
+++ apapmedikaweb/src/test/java/com/tk/apapmedikaweb/ApapmedikawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tk.apapmedikaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApapmedikawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
